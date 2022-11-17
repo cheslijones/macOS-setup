5e0408ba40117c8d77bf254d150619dbbcfc5df0
Chesli Jones <cheslijones@Cheslis-MacBook-Pro.local>
2022-11-17 06:07:02 -0800

    Removed a bunch of stuff that is no longer relevant

--- src/setup.sh
+++ src/setup.sh
@@ -16,33 +16,6 @@ sudo softwareupdate --install-rosetta
 # Install rectangle
 brew install --cask rectangle
 
-# Install iSstp
-# curl -L https://github.com/axot/isstp/releases/download/v1.4.1-rc1/iSstp.v1.4.1-rc1.zip > ~/Downloads/iSSTP.zip && \
-#    unzip ~/Downloads/iSSTP.zip -d /Applications && \
-#    rm -rf ~/Downloads/iSSTP.zip
-
-# Install parallels
-# brew install --cask parallels
-
-# Install Microsoft office
-# brew install --cask microsoft-office
-
-# Install Microsoft Teams
-# brew install --cask microsoft-teams
-
-# Install zoom
-# brew install --cask zoom
-
-# Install PIA
-brew install --cask private-internet-access
-
-# Download Logi Options
-curl -L https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.zip > ~/Downloads/options.zip && \
-    unzip ~/Downloads/options.zip -d ~/Downloads && \
-    rm -rf ~/Downloads/options.zip
-
-# Install font
-# cp "Meslo LG M Regular for Powerline.ttf" /Library/Fonts
 
 ########################
 ### WEB DEV & DEVOPS ###
@@ -60,7 +33,7 @@ brew install --cask postman
 brew install --cask pgadmin4
 
 # Install Azure CLI
-# brew install azure-cli
+brew install azure-cli
 
 # Install docker
 brew install --cask docker
@@ -83,6 +56,7 @@ brew install node
 # Install node
 brew install poetry
 
+
 ################
 ### GAME DEV ###
 ################
@@ -95,43 +69,6 @@ brew install --cask dotnet-sdk
 # Install mono
 brew install mono
 
-########################################
-### MUSIC                            ###
-### Need to download these manually: ###
-### - Logic Pro X                    ###
-### - Toontrack Product Manager      ###
-### - VSL                            ###
-### - Line 6                         ###
-########################################
-# Install FL Studio
-brew install --cask fl-studio
-
-# Install Native Access
-brew install --cask native-access
-
-# Install Arturia Software Center
-curl -L https://downloads.arturia.net/products/asc/soft/Arturia_Software_Center__2_4_0_1764.pkg > ~/Downloads/arturia.pkg && \
-    sudo installer -pkg ~/Downloads/arturia.pkg -target / && \
-    rm -rf ~/Downloads/arturia.pkg
-
-# Download Steinberg Download Assistant
-curl -L https://www.steinberg.net/sda-mac > ~/Downloads/steinberg.dmg
-
-#############
-### GAMES ###
-#############
-# Install FF
-brew install --cask final-fantasy-xiv-online
-
-###################
-### DIRECTORIES ###
-###################
-# Make project directories
-mkdir ~/Projects
-mkdir {~/Projects/Music,~/Projects/Programming}
-mkdir {~/Projects/Music/Cubase,~/Projects/Music/FLStudio,~/Projects/Music/Logic,~/Projects/Music/Maschine} 
-mkdir {~/Projects/Programming/Personal,~/Projects/Programming/Bumticket} 
-
 
 ####################
 ### VIM SETTINGS ###
@@ -140,6 +77,7 @@ mkdir {~/Projects/Programming/Personal,~/Projects/Programming/Bumticket}
 touch ~/.vimrc
 echo ":set number" >> ~/.vimrc
 echo ":syntax on" >> ~/.vimrc
+
 
 ###########
 ### ZSH ###
--- src/zshSetup.sh
+++ src/zshSetup.sh
@@ -15,23 +15,19 @@ git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/z
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 		
 # Modify the ~/.zshrc
-sed -i '' 's/robbyrussell/powerlevel9k\/powerlevel9k/g' ~/.zshrc &&
-sed -i '' 's/plugins=(git)/plugins=(git virtualenv zsh-autosuggestions zsh-completions zsh-syntax-highlighting)/g' ~/.zshrc &&
-echo 'POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir rbenv vcs)' >> ~/.zshrc &&
-echo 'POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status virtualenv root_indicator background_jobs history time)' >> ~/.zshrc &&
-echo 'POWERLEVEL9K_PROMPT_ON_NEWLINE=true' >> ~/.zshrc &&
-echo 'POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%f"' >> ~/.zshrc &&
+sed -i '' 's/robbyrussell/powerlevel9k\/powerlevel9k/g' ~/.zshrc
+sed -i '' 's/plugins=(git)/plugins=(git virtualenv zsh-autosuggestions zsh-completions zsh-syntax-highlighting)/g' ~/.zshrc
+echo 'POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir rbenv vcs)' >> ~/.zshrc
+echo 'POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status virtualenv root_indicator background_jobs history time)' >> ~/.zshrc
+echo 'POWERLEVEL9K_PROMPT_ON_NEWLINE=true' >> ~/.zshrc
+echo 'POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%f"' >> ~/.zshrc
 echo 'local user_symbol="$"
 if [[ $(print -P "%#") =~ "#" ]]; then
 	user_symbol = "#"
 fi
-' >> ~/.zshrc &&
-echo 'POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%{%B%F{black}%K{yellow}%} $user_symbol%{%b%f%k%F{yellow}%} %{%f%}"' >> ~/.zshrc &&
-echo 'POWERLEVEL9K_PROMPT_ADD_NEWLINE=true' >> ~/.zshrc &&
-echo 'POWERLEVEL9K_VCS_MODIFIED_BACKGROUND="red"' >> ~/.zshrc &&
-# echo 'export PATH="/opt/homebrew/opt/openssl@1.1/bin:$PATH"' >> ~/.zshrc && 
-# echo 'export LDFLAGS="-L/opt/homebrew/opt/openssl@1.1/lib"' >> ~/.zshrc && 
-# echo 'export CPPFLAGS="-I/opt/homebrew/opt/openssl@1.1/include"' >> ~/.zshrc &&
-# echo 'export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@1.1/lib/pkgconfig"' >> ~/.zshrc
+' >> ~/.zshrc
+echo 'POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%{%B%F{black}%K{yellow}%} $user_symbol%{%b%f%k%F{yellow}%} %{%f%}"' >> ~/.zshrc
+echo 'POWERLEVEL9K_PROMPT_ADD_NEWLINE=true' >> ~/.zshrc
+echo 'POWERLEVEL9K_VCS_MODIFIED_BACKGROUND="red"' >> ~/.zshrc
 
 source ~/.zshrc
